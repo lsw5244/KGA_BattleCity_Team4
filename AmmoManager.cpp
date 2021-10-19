62e06ae21d640854a9bc00026ce637a6e29aa55e
이성우 <lsw5244@naver.com>
2021-10-19 16:47:08 +0900

    feat : AmmoManager.cpp작성
    Init, Update, Render, Release, PlayerFire함수 작성

--- AmmoManager.cpp
+++ AmmoManager.cpp
@@ -16,18 +16,69 @@ HRESULT AmmoManager::Init()
 		(*it)->SetTileInfo(tileInfo);
 	}
 
+	for (int i = 0; i < PLAYE_MAX_AMMO_COUNT; i++)
+	{
+		playerAmmos[i] = new Ammo;
+		playerAmmos[i]->Init();
+		playerAmmos[i]->SetTileInfo(tileInfo);
+	}
 
 	return S_OK;
 }
 
 void AmmoManager::Update()
 {
+	for (it = vecEnemyAmmos.begin(); it != vecEnemyAmmos.end(); it++)
+	{
+		(*it)->Update();
+	}
+
+	for (int i = 0; i < PLAYE_MAX_AMMO_COUNT; i++)
+	{
+		playerAmmos[i]->Update();
+	}
 }
 
 void AmmoManager::Render(HDC hdc)
 {
+	for (it = vecEnemyAmmos.begin(); it != vecEnemyAmmos.end(); it++)
+	{
+		(*it)->Render(hdc);
+	}
+
+	for (int i = 0; i < PLAYE_MAX_AMMO_COUNT; i++)
+	{
+		playerAmmos[i]->Render(hdc);
+	}
 }
 
 void AmmoManager::Release()
 {
+	for (it = vecEnemyAmmos.begin(); it != vecEnemyAmmos.end(); it++)
+	{
+		SAFE_RELEASE((*it));
+	}
+	vecEnemyAmmos.clear();
+
+	for (int i = 0; i < PLAYE_MAX_AMMO_COUNT; i++)
+	{
+		SAFE_RELEASE(playerAmmos[i]);
+	}
+}
+
+void AmmoManager::EnemyFire(MoveDir dir, POINTFLOAT pos)
+{
+}
+
+void AmmoManager::PlayerFire(MoveDir dir, POINTFLOAT pos)
+{
+	for (int i = 0; i < PLAYE_MAX_AMMO_COUNT; i++)
+	{
+		if (playerAmmos[i]->GetIsAlive() == false && playerAmmos[i]->GetRenderBoomEffect() == false)
+			continue;
+
+		playerAmmos[i]->Fire(dir, pos);
+		break;
+	}
+
 }
