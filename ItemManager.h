b3b053bf07a01fc69c2a1a0099aa824992c57eb5
PoPo4860 <52219454+PoPo4860@users.noreply.github.com>
2021-10-22 16:48:43 +0900

    모든 아이템 구현, (#51)
    적탱크 파괴이펙트 통과 가능

--- ItemManager.h
+++ ItemManager.h
@@ -16,11 +16,17 @@ private:
 	PlayerTank* playerTank;
 	EnemyTankManager* enemyTankManager;
 
+	bool shovelUse;
+	float shovelTime;
+	void ActiveShoveItem();
+	void SetIronBrick(int x, int y);
+	void SetBrick(int x, int y);
 protected:
 
 public:
 	void newItem();
 	void Setdata(PlayerTank& playerTank, EnemyTankManager& enemyTankManager, TILE_INFO(*tileInfo)[TILE_COUNT]);
+	void ActiveShove() { shovelUse = true, shovelTime = 0.0f; }
 	HRESULT Init();
 	void Update();
 	void Render(HDC hdc);
